e434f6ebf1e620fb3b9e4cb1177cb58340bdfae4
Mario Carrillo <marioacr94@gmail.com>
2024-05-13 13:24:29 -0600

    textures

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 using namespace sf;
+using namespace std;
 int main()
 {
-    RenderWindow window(VideoMode(200, 200), "SFML works!");
-    CircleShape shape(100.f);
-    shape.setFillColor(Color::Green);
+    RenderWindow window(VideoMode(800, 400), "SFML works!");
+    window.setFramerateLimit(60);
+    Texture yoshiTexture;
 
+    if(!yoshiTexture.loadFromFile("assets/yoshinobg.png"))
+    {
+        cout << "Error al cargar imagen" << endl;
+    }
+    yoshiTexture.setRepeated(true);
+
+    Sprite yoshi;
+    yoshi.setTexture(yoshiTexture);
+    yoshi.setTextureRect(IntRect(0,0,800,181));
+    int opacidad = 0;
+    
     while (window.isOpen())
     {
         Event event;
@@ -15,9 +28,11 @@ int main()
                 window.close();
         }
 
-        window.clear();
-        window.draw(shape);
+        window.clear(Color(51,51,51));
+        window.draw(yoshi);
+        yoshi.setColor(Color(255,255,255, opacidad % 256));
         window.display();
+        opacidad++;
     }
 
     return 0;
